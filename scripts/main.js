e2a5d110c4c3da00f5cddd0ff74e9f46e77ee887
CommandLine <71168720+Def-Try@users.noreply.github.com>
2022-04-15 15:36:15 +0300

    update part 2

--- scripts/main.js
+++ scripts/main.js
@@ -5,207 +5,178 @@ const ui = require("ui-lib/library");
 ui.addButton("toggleparkour", "host", () => lock = !lock);
 
 Log.info("Loading variables");
-var lock = true;
-var jump = false;
+var gravity = .5; // скорость гравитации
+var bjumpvel = 15; // скорость прыжка
+var ajumpvel = 0; // доп. скорость прыжка
+var direction = 0; // 0 - Y, 1 - X
+var lock = true; // системная блокировка
+var stamina = 1000; // выносливость
+var onfloor = false; 
+var gravdirect = 3;
 var hold = false;
-var corrupt = false;
-
-var gravity = -.5;
-var ograv = gravity;
-var jumpvel = 10;
-var ojv = jumpvel;
-
-var stamina = 1000;
+var lastx;
+var lasty;
+var ltilex;
+var ltiley;
 var unit;
-var lastx = 0;
-var lasty = 0;
-var ltilex = 0;
-var ltiley = 0;
-
-var floorup;
-var floordown;
-var floor;
-
-var j;
-var lastvel = 0;
-var entime = 0;
-
-var checked = {};
-
 Log.info("Loading main content");
-
-var equalsTop = (block) => floorup != null && floorup.block() == block;
-var equalsBot = (block) => floordown != null && floordown.block() == block;
-var equals = (block) => floor != null && floor.block() == block;
-
-var checkBlocks = (yrange, xoffset) => {
-    for (j = 0; j < yrange - 1; j++) {
-        floor = Vars.world.tile(unit.tileX() + xoffset, unit.tileY() - 1 + j);
-        if (floor != null && floor.block() != Blocks.air) {
-            checked[j] = 1;
-        } else {
-            checked[j] = 0;
-        }
+var getBlock = (x, y) => {var block = Vars.world.tile(x, y);if(block != null && block.block() != Blocks.air){return block.block();}else{return false;}}
+var getTile = (x, y) => {var block = Vars.world.tile(x, y);if(block != null){return block;}else{return false;}}
+var setGravity = (grav) => {gravity = grav; jump = -grav*10}
+var updateHud = () => {Vars.ui.showInfoToast("Выносливость:" + stamina / 10 + "%", .04);}
+var getBlockBot = () => {
+    if(gravdirect==0){return Vars.world.tile(lastx+1, lasty).block()}
+    if(gravdirect==1){return Vars.world.tile(lastx, lasty+1).block()}
+    if(gravdirect==2){return Vars.world.tile(lastx-1, lasty).block()}
+    if(gravdirect==3){return Vars.world.tile(lastx, lasty-1).block()}
+}
+var updateFloor = () => { 
+    if(gravdirect == 0){
+        if(getBlock(lastx+1, lasty).solid){ if(stamina<1000){stamina += 100}else{stamina=1000}; onfloor = true; ltilex = lastx; ltiley = lasty;}else{onfloor=false;}
+    }else if(gravdirect == 2){
+        if(getBlock(lastx-1, lasty).solid){ if(stamina<1000){stamina += 100}else{stamina=1000}; onfloor = true; ltilex = lastx; ltiley = lasty;}else{onfloor=false;}
+    }else if(gravdirect == 1){
+        if(getBlock(lastx, lasty+1).solid){ if(stamina<1000){stamina += 100}else{stamina=1000}; onfloor = true; ltilex = lastx; ltiley = lasty;}else{onfloor=false;}
+    }else{
+        if(getBlock(lastx, lasty-1).solid){ if(stamina<1000){stamina += 100}else{stamina=1000}; onfloor = true; ltilex = lastx; ltiley = lasty;}else{onfloor=false;}
     }
-    return checked;
-};
-
-var checkBlock = (block, yrange, xrange) => {
-    if (block != null && block.block() != Blocks.air) {
-        return block.block();
-    } else {
-        return false;
-    }
-};
-
-var updateHud = () => {
-    if (corrupt == false) Vars.ui.showInfoToast("Stamina:" + stamina / 10 + "%", .04);
-    if (corrupt) Vars.ui.showInfoToast("[scarlet]Corruption detected!", .04);
-};
-
-var update = () => {
-    unit = Vars.player.unit();
-    if (unit == null) return;
-
+}
+var updateGravity = () => {
+    if(gravdirect==0){unit.vel.add(gravity, 0);}
+    if(gravdirect==1){unit.vel.add(0, gravity);}
+    if(gravdirect==2){unit.vel.add(-gravity, 0);}
+    if(gravdirect==3){unit.vel.add(0, -gravity);}
+}
+var jump = (vel) => {
+    if(gravdirect==0){unit.vel.add(-vel, 0);}
+    if(gravdirect==1){unit.vel.add(0, -vel);}
+    if(gravdirect==2){unit.vel.add(vel, 0);}
+    if(gravdirect==3){unit.vel.add(0, vel);}
+}
+//ЭТОТ РЕГИОН ДЛЯ ФУНКЦИЙ МЕХАНИК!
+var gravipad = (unit) => {
     lastx = unit.tileX();
     lasty = unit.tileY();
-
-    floorup = Vars.world.tile(lastx, lasty + 1);
-    floordown = Vars.world.tile(lastx, lasty - 1);
-    floor = Vars.world.tile(lastx, lasty);
-
-    if (Core.input.keyDown(KeyCode.controlLeft) && Core.input.keyTap(KeyCode.l)) lock = !lock;
-    if (hold == false) {
-        if (stamina > 100) {
-            unit.vel.add(0, Core.input.keyTap(Binding.pause) && jump ? jumpvel : gravity);
-        } else {
-            unit.vel.add(0, gravity);
+    if(getBlock(lastx, lasty)==Blocks.conveyor){ // гравипад
+        if(getTile(lastx, lasty).build.rotation==0){ // гравитация вправо
+            gravdirect = 0;
+        }else if(getTile(lastx, lasty).build.rotation==1){ // гравитация вверх
+            gravdirect = 1;
+        }else if(getTile(lastx, lasty).build.rotation==2){ // гравитация влево
+            gravdirect = 2;
+        }else if(getTile(lastx, lasty).build.rotation==3){ // гравитация вниз
+            gravdirect = 3;
         }
-    } else {
-        unit.vel.add(0, Core.input.keyTap(Binding.pause) && jump ? jumpvel : 0);
-    }
-    if (Core.input.keyTap(Binding.pause) && jump && stamina > 100) { jump = false; stamina = stamina - 100; }
-    if (!equalsTop(Blocks.air) || !equalsBot(Blocks.air)) {
-        try {
-            if (equalsTop(Blocks.titaniumWallLarge) || equalsBot(Blocks.titaniumWallLarge)) {
-                if (ltiley == lasty) {
-                    if (gravity > 0) {
-                        jumpvel = -15;
-                        ojv = -15;
-                    } else {
-                        jumpvel = 15;
-                        ojv = 15;
-                    }
-                } else {
-                    if (gravity > 0) {
-                        unit.vel.add(0, lasty - ltiley - 15);
-                    } else {
-                        unit.vel.add(0, ltiley - lasty + 15);
-                    }
-                }
-            } else if (equalsTop(Blocks.phaseWallLarge) || equalsBot(Blocks.phaseWallLarge)) {
-                if (Core.input.keyDown(KeyCode.a)) {
-                    unit.vel.add(-0.5, 0);
-                    lastvel = unit.vel.x
-                } else if (Core.input.keyDown(KeyCode.d)) {
-                    unit.vel.add(0.5, 0);
-                    lastvel = unit.vel.x
-                }
-            } else {
-                if (gravity > 0) {
-                    jumpvel = -10;
-                    ojv = -10;
-                } else {
-                    jumpvel = 10;
-                    ojv = 10;
-                }
-                ltilex = unit.tileX();
-                ltiley = unit.tileY();
+    } 
+}
+var gelJump = (unit) => {
+    lastx = unit.tileX();
+    lasty = unit.tileY();
+    if(getBlockBot()==Blocks.titaniumWall){
+        if(gravdirect == 0 || gravdirect == 2){
+            if(ltilex == lastx){
+                ajumpvel = 15;
+            }else{
+                override = false;
+                ajumpvel = 0;
+                jump(bjumpvel+15);
             }
-            jump = true;
-            stamina = Math.min(stamina + 50, 1000);
-        } catch (e) { Log.info("parkour-mod: " + e) }
-    }
-    if (checkBlock(floor)) {
-        if (equals(Blocks.conveyor)) {
-            if (gravity > 0 && !equalsTop(Blocks.air)) {
-                jumpvel = 10;
-                gravity = -.5;
-                ograv = gravity;
-                ojv = jumpvel;
-            } else if (gravity < 0 && !equalsBot(Blocks.air)) {
-                jumpvel = -10;
-                gravity = .5;
-                ograv = gravity;
-                ojv = jumpvel;
-            };
-        } else if (equals(Blocks.titaniumConveyor)) {
-            if (Core.input.keyTap(Binding.pause)) {
-                if (gravity > 0 && !equalsTop(Blocks.air)) {
-                    jumpvel = 10;
-                    gravity = -.5;
-                    ograv = gravity;
-                    ojv = jumpvel;
-                } else if (gravity < 0 && !equalsBot(Blocks.air)) {
-                    jumpvel = -10;
-                    gravity = .5;
-                    ograv = gravity;
-                    ojv = jumpvel;
-                };
+        }else{
+            if(ltiley == lasty){
+                ajumpvel = 15;
+            }else{
+                ajumpvel = 0;
+                jump(bjumpvel+15);
             }
-        } else if (equals(Blocks.plastaniumConveyor)) {
-            stamina = 1000;
-        } else if (equals(Blocks.shockMine)) {
-            if (gravity != 0) {
-                ograv = gravity;
-                ojv = jumpvel;
-            }
-            gravity = 0;
-            jumpvel = 0;
-        } else if (equals(Blocks.duct)) {
-            Vars.ui.announce("[#0099ff]Toggled parkour mode");
-            lock = !lock;
-        } else if (equals(Blocks.conduit)) {
-            if (Core.input.keyDown(KeyCode.a)) {
-                unit.vel.add(.5, 0);
-            } else if (Core.input.keyDown(KeyCode.d)) {
-                unit.vel.add(-.5, 0);
-            }
-            if (Mathf.chance((Time.time - entime) / 100000)) {
-                unit.vel.add(Math.random() * (1 - -1), 0);
-            }
-            corrupt = true;
-            if (entime == 0) entime = Time.time
-        } else {
-            gravity = ograv;
-            jumpvel = ojv;
-            corrupt = false;
-            entime = 0;
-        };
-    } else {
-        gravity = ograv;
-        jumpvel = ojv;
-        corrupt = false;
-    };
-    if ((checkBlocks(3, -1)[0] == 1 || checkBlock(Vars.world.tile(lastx - 1, lasty)) != false) && Core.input.keyDown(Binding.pause) && stamina > 0 && !corrupt) {
-        stamina -= 50;
+        }
+    }else{ajumpvel = 0;}
+}
+var gelStick = (unit) => {
+    lastx = unit.tileX();
+    lasty = unit.tileY();
+    if(getBlock(lastx+1, lasty)==Blocks.plastaniumWall){
         hold = true;
-    } else if ((checkBlocks(3, 1)[0] == 1 || checkBlock(Vars.world.tile(lastx + 1, lasty)) != false) && Core.input.keyDown(Binding.pause) && stamina > 0 && !corrupt) {
-        stamina -= 50;
+        if(Core.input.keyTap(Binding.pause) && stamina > 99){unit.vel.add(-15, 0);}
+    }else if(getBlock(lastx-1, lasty)==Blocks.plastaniumWall){
         hold = true;
-    } else {
-        hold = false;
+        if(Core.input.keyTap(Binding.pause) && stamina > 99){unit.vel.add(15, 0);}
+    }else if(getBlock(lastx, lasty+1)==Blocks.plastaniumWall){
+        hold = true;
+        if(Core.input.keyTap(Binding.pause) && stamina > 99){unit.vel.add(0, -15);}
+    }else if(getBlock(lastx, lasty-1)==Blocks.plastaniumWall){
+        hold = true;
+        if(Core.input.keyTap(Binding.pause) && stamina > 99){unit.vel.add(0, 15);}
     }
-    if (stamina <= 0) hold = false;
-    unit.vel.add(lastvel, 0);
-    lastvel = lastvel > 0 ? lastvel - .15 : 0;
-};
+}
+var wallHolding = (unit) => {
+    if(Core.input.keyDown(Binding.pause)){
+        if(stamina>99){
+            if(getBlock(lastx+1, lasty)!=false){
+                hold = true;
+            }else if(getBlock(lastx-1, lasty)!=false){
+                hold = true;
+            }else if(getBlock(lastx, lasty+1)!=false){
+                hold = true;
+            }else if(getBlock(lastx, lasty-1)!=false){
+                hold = true;
+            }
+        }else{
+            hold = false;
+        }
+    }
+}
+var graviFunnel = (unit) => {
+    lastx = unit.tileX();
+    lasty = unit.tileY();
+    if(getBlock(lastx, lasty)==Blocks.pulseConduit){ // грави воронка
+        hold = true;
+        if(getTile(lastx, lasty).build.rotation==0){
+            unit.vel.add(.05, 0);
+        }else if(getTile(lastx, lasty).build.rotation==1){
+            unit.vel.add(0, .05);
+        }else if(getTile(lastx, lasty).build.rotation==2){
+            unit.vel.add(-.05, 0);
+        }else if(getTile(lastx, lasty).build.rotation==3){
+            unit.vel.add(0, -.05);
+        }
+    }
+}
+var antiGravField = (unit) => {
+    lastx = unit.tileX();
+    lasty = unit.tileY();
+    if(getBlock(lastx, lasty)==Blocks.shockMine){
+        hold = true;
+    } 
+}
 
+//конец региона механик
+var update = () => { // главный цикл
+    unit = Vars.player.unit();
+    if (unit == null) return;
+    try{
+        lastx = unit.tileX();
+        lasty = unit.tileY();
+        if(Core.input.keyTap(Binding.pause) && stamina > 99 && onfloor){jump(bjumpvel+ajumpvel); stamina -= 100; } // работа прыжка
+        gravipad(unit);
+        gelJump(unit);
+        gelStick(unit);
+        wallHolding(unit);
+        graviFunnel(unit);
+        antiGravField(unit);
+        if(!hold) updateGravity();
+    }catch(e){
+        Log.info("parkour-mod: " + e + ". Maybe you in the void?")
+    }
+    hold = false;
+    
+
+};
 Log.info("Running update task");
 Timer.schedule(() => {
     if (lock) return;
     update();
     updateHud();
+    updateFloor();
 }, 0, .02);
 
 Log.info("Done initialisation of parkour-mod.");
